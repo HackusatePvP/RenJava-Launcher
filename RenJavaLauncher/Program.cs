6edc9d8973cfedd6eac342bfbf735c7e4fe23998
Hackusate_PvP <Supermftw@gmail.com>
2024-05-06 09:50:23 -0500

    Add project files.

---
+++ RenJavaLauncher/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        String directoryPath = Directory.GetCurrentDirectory();
+        Console.WriteLine("Current Directory: " + directoryPath);
+        DirectoryInfo directoryInfo = new DirectoryInfo(directoryPath);
+        FileInfo[] files = directoryInfo.GetFiles("*.jar");
+        foreach (FileInfo file in files)
+        {
+            Console.WriteLine("Executing jar: " + file.Name);
+            var processInfo = new ProcessStartInfo(Directory.GetCurrentDirectory() + "\\jdk\\windows\\bin\\javaw.exe", "-jar " + file.Name)
+            {
+                CreateNoWindow = true,
+                UseShellExecute = true,
+            };
+            if (processInfo != null)
+            {
+                Process process = Process.Start(processInfo);
+            }
+            break;
+        }
+    }
+}
